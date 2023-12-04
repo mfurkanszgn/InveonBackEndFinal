014c6987c0728f8f10dcc5a190e2619eed6f4cb5
mfurkanszgn <furkan.sezgin45@gmail.com>
2023-12-04 23:15:55 +0300

    Fİnal

---
+++ Inveon_Final_BackEnd-main/microservices/Inveon.GatewaySolution/Extensions/CorsPolicy.cs
@@ -0,0 +1,15 @@
+﻿namespace Inveon.GatewaySolution.Extensions
+{
+    public static class CorsPolicy
+    {
+        public static IServiceCollection configureCors(this IServiceCollection services)
+        {
+            services.AddCors(options => options.AddPolicy("ApiCorsPolicy", builder =>
+            {
+                builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+            }
+            ));
+            return services;
+        }
+    }
+}
